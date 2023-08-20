c22acfe1cc97339055c568823193de828724ab6e
Ritaban Mitra <115375296+ritabanm-99@users.noreply.github.com>
2023-08-19 22:57:21 -0400

    Add files via upload
    updated the app icons.

---
+++ Caffe/Caffe/CaffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeApp.swift
+//  Caffe
+//
+//  Created by Ritaban Mitra on 03/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CaffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
